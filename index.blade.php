212ccb64f569a8e5f1342abf0da5638c8207ba44
maharaninav <168819264+maharaninav@users.noreply.github.com>
2024-07-18 23:51:39 +0700

    layouts profile private

--- index.blade.php
+++ index.blade.php
@@ -5,14 +5,16 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Data Home</div>
+                <div class="card-header">Data Profile</div>
                 <div class="container">
                     <div class="row mb-3">
                         <div class="col-8">
                             <h4 class="card-title">Add Data</h4>
                         </div>
                         <div class="col-4 d-flex justify-content-end">
-                            <a href="{{ route('home.create') }}" class="btn" style="background-color: #0DCA37; color:white;"><i class="fa fa-fw fa-plus"></i> Add</a>
+                            <a href="{{ route('profile.create') }}" class="btn" style="background-color: #0DCA37; color:white;">
+                                <i class="fa fa-fw fa-plus"></i> Add
+                            </a>
                         </div>
                     </div>
 
@@ -20,31 +22,27 @@
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th>About</th>
-                                    <th>Expertise</th>
+                                    <th>Education</th>
                                     <th>Image</th>
+                                    <th>Passion</th>
+                                    <th>Image 2</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($homes as $home)
+                                @foreach($profiles as $profile)
                                 <tr>
-                                    <td>{{ $home->about }}</td>
-                                    <td>{{ $home->expertise }}</td>
+                                    <td>{{ $profile->education }}</td>
+                                    <td><img src="{{ Storage::url($profile->image) }}" alt="Image" style="width: 100px;"></td>
+                                    <td>{{ $profile->passion }}</td>
+                                    <td><img src="{{ Storage::url($profile->image2) }}" alt="Image 2" style="width: 100px;"></td>
                                     <td>
-                                        @if($home->image)
-                                            <img src="{{ Storage::url($home->image) }}" alt="Home Image" style="width: 100px;">
-                                        @else
-                                            No image
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <form action="{{ route('home.destroy', $home->id) }}" method="POST">
+                                        <form action="{{ route('profile.destroy', $profile->id) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger">Delete</button>
                                         </form>
-                                        <a href="{{ route('home.edit', $home->id) }}" class="btn btn-primary">Edit</a>
+                                        <a href="{{ route('profile.edit', $profile->id) }}" class="btn btn-primary">Edit</a>
                                     </td>
                                 </tr>
                                 @endforeach
