b8d342598a649c74feb8e3e777b6d3b44105f93f
maharaninav <168819264+maharaninav@users.noreply.github.com>
2024-07-18 23:48:36 +0700

    layouts private

---
+++ dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.private.app')
+@section('content')
+
+              <!-- Begin Page Content -->
+               <div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+
+</div>
+    <h2 class="text-gray-800">Selamat datang Admin!!</h2>
+
+
+@endsection
