eaf466ab0df8e20c7d27696bb2fc464c7fab275c
maharaninav <168819264+maharaninav@users.noreply.github.com>
2024-07-18 22:53:52 +0700

    Dashboardcontroller.php

---
+++ DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class DashboardController extends Controller
+{
+    //
+    public function index() {
+        $data = array (
+            'title' => 'Home',
+            'user' => Auth::user() 
+        );
+        return view('layouts.private.dashboard', $data);
+    }
+}
