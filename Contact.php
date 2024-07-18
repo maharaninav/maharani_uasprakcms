b880e2ec0e430821db65083722026930e4cd3215
maharaninav <168819264+maharaninav@users.noreply.github.com>
2024-07-18 23:11:57 +0700

    Contact model.php

---
+++ Contact.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $table = "contact";
+    protected $fillable = ['name', 'email', 'phone_number', 'contact_message'];
+    public $timestamps = false;
+
+}
